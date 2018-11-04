be8024d6b34701f4e1291986d3ad81e799a46fb3
NadirJoulali <39174113+NadirJoulali@users.noreply.github.com>
2018-11-04 16:23:43 +0000

    Update bot.js

--- bot.js
+++ bot.js
@@ -19,14 +19,32 @@ npm install queue
 */
  
 client.on('ready', () => {
-    console.log(`Logged in as ${client.user.tag}!`);
-    console.log(`in ${client.guilds.size} servers `)
-    console.log(`[Codes] ${client.users.size}`)
-    client.user.setStatus("idle")
+  console.log(`Logged in as ${client.user.tag}!`);
+client.user.setGame(`/play || / V2.1`,"http://twitch.tv/Nadir44king")
+  console.log('')
+  console.log('')
+  console.log('╔[═════════════════════════════════════════════════════════════════]╗')
+  console.log(`[Start] ${new Date()}`);
+  console.log('╚[═════════════════════════════════════════════════════════════════]╝')
+  console.log('')
+  console.log('╔[════════════════════════════════════]╗');
+  console.log(`Logged in as * [ " ${client.user.username} " ]`);
+  console.log('')
+  console.log('Informations :')
+  console.log('')
+  console.log(`servers! [ " ${client.guilds.size} " ]`);
+  console.log(`Users! [ " ${client.users.size} " ]`);
+  console.log(`channels! [ " ${client.channels.size} " ]`);
+  console.log('╚[════════════════════════════════════]╝')
+  console.log('')
+  console.log('╔[════════════]╗')
+  console.log(' Bot Matra9a Is Online')
+  console.log('╚[════════════]╝')
+  console.log('')
+  console.log('')
 });
-client.on('ready', () => {
-     client.user.setActivity("you",{type: 'WATCHING'});
- 
+
+
 });
 const prefix = "."
 client.on('message', async msg => {
